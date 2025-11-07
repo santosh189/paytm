45ae1abf47f8e35a907c59435f36fe38aa7a71d5
santosh189 <61221140+santosh189@users.noreply.github.com>
2025-11-07 18:02:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 Hello i am in DEV branch.......
 this file contains deployment details...
 Deployment details are present
-content
-misss
-hndscnkldsn
-lmvklfdnv'
-kdvkfv;'fd;',dfv.
-'
-alvmldf,vlm
